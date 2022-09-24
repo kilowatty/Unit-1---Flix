e0228d9e469f0c91dfd9086629463e1f32a45e82
kilowatty <74945640+kilowatty@users.noreply.github.com>
2022-09-24 00:12:04 -0400

    Flix Part 2

--- Flix/MovieCell.swift
+++ Flix/MovieCell.swift
@@ -8,12 +8,10 @@
 import UIKit
 
 class MovieCell: UITableViewCell {
+    
 
     @IBOutlet weak var titleLabel: UILabel!
-    
-    
     @IBOutlet weak var synopsisLabel: UILabel!
-    
     @IBOutlet weak var posterView: UIImageView!
     
     
---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Jessica Kent on 9/22/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
